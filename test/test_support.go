56940978ddda307d8465497fce6c1c49327d8d06
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-08-10 13:21:26 -0700

    Fix bug in test_support. Didnt need those functions

--- test/test_support.go
+++ test/test_support.go
@@ -62,34 +62,3 @@ type AllTypes struct {
 	String  string
 	*zoom.Model
 }
-
-// Database helper functions
-// setUp() and tearDown()
-func setUp() error {
-	zoom.Init(&zoom.Configuration{Database: 9})
-
-	conn := zoom.GetConn()
-	defer conn.Close()
-
-	n, err := redis.Int(conn.Do("DBSIZE"))
-	if err != nil {
-		return err
-	}
-
-	if n != 0 {
-		return errors.New("Database #9 is not empty, test can not continue")
-	}
-
-	zoom.Register(&Person{}, "person")
-	return nil
-}
-
-func tearDown() {
-	conn := zoom.GetConn()
-	_, err := conn.Do("flushdb")
-	if err != nil {
-		log.Fatal(err)
-	}
-	conn.Close()
-	zoom.Close()
-}
