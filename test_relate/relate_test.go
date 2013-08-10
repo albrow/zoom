3b5d8ca2f6a332e1d8f5103839e6d5dd5987d781
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-08-09 22:44:37 -0700

    Add some tests for relations

--- test_relate/relate_test.go
+++ test_relate/relate_test.go
@@ -23,15 +23,50 @@ func (s *RelateSuite) SetUpSuite(c *C) {
 	if err != nil {
 		c.Error(err)
 	}
+
+	err = zoom.Register(&Pet{}, "pet")
+	if err != nil {
+		c.Error(err)
+	}
 }
 
 func (s *RelateSuite) TearDownSuite(c *C) {
+
 	zoom.UnregisterName("person")
+	zoom.UnregisterName("pet")
+
 	conn := zoom.GetConn()
 	_, err := conn.Do("flushdb")
 	if err != nil {
 		c.Error(err)
 	}
 	conn.Close()
+
 	zoom.Close()
+}
+
+func (s *RelateSuite) TestOneToOne(c *C) {
+	person := NewPerson("Alex", 20)
+	pet := NewPet("Billy", "barracuda")
+
+	person.Pet = pet
+	err := zoom.Save(person)
+	if err != nil {
+		c.Error(err)
+	}
+
+	// result, err := zoom.FindById("person", person.Id)
+	// if err != nil {
+	// 	c.Error(err)
+	// }
+
+	// person2, ok := result.(*Person)
+	// if !ok {
+	// 	c.Error("Couldn't type assert to *Person: ", person2)
+	// }
+
+	// pet2 := person2.Pet
+	// c.Assert(pet2, NotNil)
+	// c.Assert(pet2.Name, Equals, "Billy")
+	// c.Assert(pet2.Kind, Equals, "barracuda")
 }
