0fb48762bb035be078075d9e07784a3374d458b8
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-09-04 15:57:37 -0400

    refactor testing structure. Add some tests in model_test.go

---
+++ support/types.go
@@ -0,0 +1,35 @@
+// file contains type declarations that are used in various places, especially in testspackage test
+
+package support
+
+import (
+	"github.com/stephenalexbrowne/zoom"
+)
+
+// The Person struct
+type Person struct {
+	Name string
+	Age  int
+	zoom.DefaultData
+}
+
+// The AllTypes struct
+// A struct containing all supported types
+type AllTypes struct {
+	Uint    uint
+	Uint8   uint8
+	Uint16  uint16
+	Uint32  uint32
+	Uint64  uint64
+	Int     int
+	Int8    int8
+	Int16   int16
+	Int32   int32
+	Int64   int64
+	Float32 float32
+	Float64 float64
+	Byte    byte
+	Rune    rune
+	String  string
+	zoom.DefaultData
+}
--- test/zoom_test.go
+++ test/zoom_test.go
@@ -1,5 +1,6 @@
 package test
 
 // import (
+// 	"github.com/stephenalexbrowne/zoom/support"
 // 	"testing"
 // )
