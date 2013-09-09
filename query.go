3cfe70023a511b5d3deb1002f3a25f9353621888
albrow <stephenalexbrowne@gmail.com>
2013-09-09 00:49:54 -0400

    Add a MultiQuery object used for FindAll. Implement and Test Sort, Limit, and Offset options.

--- query.go
+++ query.go
@@ -6,6 +6,7 @@ package zoom
 import (
 	"errors"
 	"fmt"
+	"github.com/stephenalexbrowne/zoom/redis"
 	"github.com/stephenalexbrowne/zoom/util"
 	"reflect"
 )
@@ -17,6 +18,57 @@ type Query struct {
 	modelName string
 	id        string
 	err       error
+}
+
+type MultiQuery struct {
+	scannables []Model
+	includes   []string
+	excludes   []string
+	modelName  string
+	sort       sort
+	limit      uint
+	offset     uint
+	err        error
+}
+
+type sort struct {
+	fieldName string
+	desc      bool
+	alpha     bool
+}
+
+type includerExcluder interface {
+	getIncludes() []string
+	getExcludes() []string
+}
+
+type namedIncluderExcluder interface {
+	includerExcluder
+	name() string
+}
+
+func (q *Query) getIncludes() []string {
+	return q.includes
+}
+
+func (q *Query) getExcludes() []string {
+	return q.excludes
+}
+
+func (q *MultiQuery) getIncludes() []string {
+	return q.includes
+}
+
+func (q *MultiQuery) getExcludes() []string {
+	return q.excludes
+}
+
+func (q *Query) name() string {
+	return q.modelName
+}
+
+func (q *MultiQuery) name() string {
+	return q.modelName
 }
 
 func FindById(modelName, id string) *Query {
@@ -102,29 +154,8 @@ func (q *Query) Exec() (Model, error) {
 	t := newTransaction()
 
 	// set up includes
-	ms := modelSpecs[q.modelName]
-	includes := ms.fieldNames
-	if len(q.includes) != 0 {
-		includes = q.includes
-		fmt.Println("includes: ", includes)
-		// add a model find operation to the transaction
-		if err := t.findModelWithIncludes(q.modelName, q.id, q.scannable, includes); err != nil {
-			return q.scannable, err
-		}
-	} else if len(q.excludes) != 0 {
-		for _, name := range q.excludes {
-			includes = util.RemoveElementFromStringSlice(includes, name)
-		}
-		fmt.Println("includes: ", includes)
-		// add a model find operation to the transaction
-		if err := t.findModelWithIncludes(q.modelName, q.id, q.scannable, includes); err != nil {
-			return q.scannable, err
-		}
-	} else {
-		// add a model find operation to the transaction
-		if err := t.findModel(q.modelName, q.id, q.scannable); err != nil {
-			return q.scannable, err
-		}
+	if err := executeModelFindWithIncludes(q.id, q.scannable, q, t); err != nil {
+		return q.scannable, err
 	}
 
 	// execute the transaction
@@ -133,4 +164,181 @@ func (q *Query) Exec() (Model, error) {
 	}
 
 	return q.scannable, nil
+}
+
+func executeModelFindWithIncludes(id string, scannable Model, q namedIncluderExcluder, t *transaction) error {
+	ms := modelSpecs[q.name()]
+	includes := ms.fieldNames
+	if len(q.getIncludes()) != 0 {
+		// add a model find operation to the transaction
+		if err := t.findModelWithIncludes(q.name(), id, scannable, q.getIncludes()); err != nil {
+			return err
+		}
+	} else if len(q.getExcludes()) != 0 {
+		for _, name := range q.getExcludes() {
+			includes = util.RemoveElementFromStringSlice(includes, name)
+		}
+		// add a model find operation to the transaction
+		if err := t.findModelWithIncludes(q.name(), id, scannable, includes); err != nil {
+			return err
+		}
+	} else {
+		// add a model find operation to the transaction
+		if err := t.findModel(q.name(), id, scannable); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func FindAll(modelName string) *MultiQuery {
+	// create and return a query object
+	return &MultiQuery{
+		modelName: modelName,
+	}
+}
+
+func (q *MultiQuery) Include(fields ...string) *MultiQuery {
+	if len(q.excludes) > 0 {
+		q.setErrorIfNone(errors.New("zoom: cannot use both Include and Exclude modifiers on a query"))
+		return q
+	}
+	q.includes = append(q.includes, fields...)
+	return q
+}
+
+func (q *MultiQuery) Exclude(fields ...string) *MultiQuery {
+	if len(q.includes) > 0 {
+		q.setErrorIfNone(errors.New("zoom: cannot use both Include and Exclude modifiers on a query"))
+		return q
+	}
+	q.excludes = append(q.excludes, fields...)
+	return q
+}
+
+func (q *MultiQuery) SortBy(field string) *MultiQuery {
+	q.sort.fieldName = field
+	return q
+}
+
+func (q *MultiQuery) Order(order string) *MultiQuery {
+	if order == "ASC" {
+		q.sort.desc = false
+	} else if order == "DESC" {
+		q.sort.desc = true
+	} else {
+		q.setErrorIfNone(errors.New("zoom: order must be either ASC or DESC"))
+	}
+	return q
+}
+
+func (q *MultiQuery) Limit(amount uint) *MultiQuery {
+	q.limit = amount
+	return q
+}
+
+func (q *MultiQuery) Offset(amount uint) *MultiQuery {
+	q.offset = amount
+	return q
+}
+
+func (q *MultiQuery) setErrorIfNone(e error) {
+	if q.err == nil {
+		q.err = e
+	}
+}
+
+func (q *MultiQuery) Exec() ([]Model, error) {
+	// check if the query had any prior errors
+	if q.err != nil {
+		return q.scannables, q.err
+	}
+
+	// get the type corresponding to the modelName
+	typ, err := getRegisteredTypeFromName(q.modelName)
+	if err != nil {
+		return nil, err
+	}
+
+	// get the ids for the models
+	ids, err := q.getIds(typ)
+	if err != nil {
+		return nil, err
+	}
+
+	// start a transaction
+	t := newTransaction()
+
+	// allocate a slice of Model
+	q.scannables = make([]Model, len(ids))
+
+	// iterate through the ids and add a find operation for each model
+	for i, id := range ids {
+
+		// instantiate a model using reflection
+		modelVal := reflect.New(typ.Elem())
+		m, ok := modelVal.Interface().(Model)
+		if !ok {
+			msg := fmt.Sprintf("zoom: could not convert val of type %T to Model", modelVal.Interface())
+			return nil, errors.New(msg)
+		}
+
+		// set the ith element of scannables
+		q.scannables[i] = m
+
+		// add a find operation for the model m
+		executeModelFindWithIncludes(id, q.scannables[i], q, t)
+	}
+
+	// execute the transaction
+	if err := t.exec(); err != nil {
+		return nil, err
+	}
+
+	return q.scannables, nil
+}
+
+func (q *MultiQuery) getIds(typ reflect.Type) ([]string, error) {
+
+	// get a connection
+	conn := GetConn()
+	defer conn.Close()
+
+	// construct a redis command to get the ids
+	indexKey := q.modelName + ":index"
+	args := redis.Args{}
+	var command string
+	if q.sort.fieldName == "" {
+		// without sorting sorting
+		command = "SMEMBERS"
+		args = args.Add(indexKey)
+	} else {
+		// with sorting
+		command = "SORT"
+		weight := q.modelName + ":*->" + q.sort.fieldName
+		args = args.Add(indexKey).Add("BY").Add(weight)
+
+		// figure out if we need the alpha option
+		field, found := typ.Elem().FieldByName(q.sort.fieldName)
+		if !found {
+			msg := fmt.Sprintf("zoom: invalid SortBy modifier. model of type %s has no field %s\n.", typ.String(), q.sort.fieldName)
+			return nil, errors.New(msg)
+		}
+		if field.Type.Kind() == reflect.String {
+			args = args.Add("ALPHA")
+		}
+
+		// add either ASC or DESC
+		if q.sort.desc {
+			args = args.Add("DESC")
+		} else {
+			args = args.Add("ASC")
+		}
+
+		// add limit if applicable
+		if q.limit != 0 {
+			args = args.Add("LIMIT").Add(q.offset).Add(q.limit)
+		}
+	}
+	return redis.Strings(conn.Do(command, args...))
 }
