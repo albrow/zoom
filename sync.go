424aa6d49742b8a7ec8422bdf75ba5ee1f605ecb
albrow <stephenalexbrowne@gmail.com>
2014-08-11 22:09:07 -0400

    Make GetMutexById public and add godoc comments to sync.go

--- sync.go
+++ sync.go
@@ -7,19 +7,24 @@ import (
 var mutexMap = make(map[string]*sync.Mutex)
 var globalMutexLock sync.Mutex
 
-func getMutexForId(id string) *sync.Mutex {
+// GetMutexById returns a mutex identified by mutexId. It will always
+// return the same mutex given the same mutexId. It can be used to manually
+// lock and unlock references to specific redis keys for stricter thread-safety.
+func GetMutexById(mutexId string) *sync.Mutex {
 	globalMutexLock.Lock()
 	defer globalMutexLock.Unlock()
-	if existing, found := mutexMap[id]; found {
+	if existing, found := mutexMap[mutexId]; found {
 		return existing
 	} else {
 		// create a new Mutex and add it to the map
 		mut := &sync.Mutex{}
-		mutexMap[id] = mut
+		mutexMap[mutexId] = mut
 		return mut
 	}
 }
 
+// Sync provides a default implementation of Syncer. It can be directly
+// embedded into model structs.
 type Sync struct {
 	MutexId string `json:"-",redis:"-"`
 }
@@ -28,20 +33,26 @@ func (s Sync) Lock() {
 	if s.MutexId == "" {
 		panic("Zoom: attempt to call Lock on a model without an Id")
 	}
-	getMutexForId(s.MutexId).Lock()
+	GetMutexById(s.MutexId).Lock()
 }
 
 func (s Sync) Unlock() {
 	if s.MutexId == "" {
 		panic("Zoom: attempt to call Unlock on a model without an Id")
 	}
-	getMutexForId(s.MutexId).Unlock()
+	GetMutexById(s.MutexId).Unlock()
 }
 
 func (s *Sync) SetMutexId(id string) {
 	s.MutexId = id
 }
 
+// Syncer is an interface meant for controlling accesss to a model in
+// a thread-safe manner. Any model that implements Syncer will automatically
+// call Lock before being retrieved from the database, so that there can effectively
+// only be one active reference to a model (identified by a mutexId) at any given
+// time. Zoom will not automatically call Unlock, however, and it is up to the
+// user call Unlock when done making changes to a specific model.
 type Syncer interface {
 	Lock()
 	Unlock()
