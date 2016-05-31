f4c07920aaefdd1113b4ecf80c9071be27dd86e7
Alex Browne <stephenalexbrowne@gmail.com>
2016-05-30 21:27:00 -0700

    Add a test for WatchKey

--- transaction_test.go
+++ transaction_test.go
@@ -38,3 +38,36 @@ func TestWatch(t *testing.T) {
 	assert.Equal(t, expectedString, other.String, "First update *was not* committed")
 	assert.Equal(t, model.Int, other.Int, "Second update *was* committed")
 }
+
+func TestWatchKey(t *testing.T) {
+	testingSetUp()
+	defer testingTearDown()
+	conn1 := testPool.NewConn()
+	defer conn1.Close()
+	key := "mykey"
+	_, err := conn1.Do("SET", key, "foo")
+	require.NoError(t, err)
+	tx := testPool.NewTransaction()
+	// Issue a WATCH command
+	require.NoError(t, tx.WatchKey(key))
+	// Update the key directly using a different connection. This should
+	// trigger WATCH
+	expectedVal := "bar"
+	conn2 := testPool.NewConn()
+	defer conn2.Close()
+	_, err = conn2.Do("SET", key, expectedVal)
+	require.NoError(t, err)
+	// Try to update the key using the transaction. We expect this to fail
+	// and return a WatchError
+	tx.Command("SET", redis.Args{key, "should_not_be_set"}, nil)
+	err = tx.Exec()
+	assert.Error(t, err)
+	assert.IsType(t, WatchError{}, err)
+	// Finally get the key to make sure the changes in the transaction were not
+	// committed.
+	conn3 := testPool.NewConn()
+	defer conn3.Close()
+	got, err := redis.String(conn3.Do("GET", key))
+	require.NoError(t, err)
+	require.Exactly(t, expectedVal, got)
+}
